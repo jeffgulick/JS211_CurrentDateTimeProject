08c1650627212c67b9b951d1818bc508e5e5f6c0
JEFFGULICK <jeffgulick@gmail.com>
2020-06-21 23:32:47 -0500

    wrote function to change string to number

--- main.js
+++ main.js
@@ -12,8 +12,6 @@ const displayDate = () => {
 
   document.getElementById("display-element").innerHTML = currentDate;
 }
- 
-
 // Write a JavaScript program to convert a number to a string.
 const numberToString = (number) => {
   if(typeof number === 'string'){
@@ -22,12 +20,19 @@ const numberToString = (number) => {
   return number.toString();
   }
 }
-
 let newString = numberToString(555)
 console.log(newString)
 console.log('your number is now a ' +  typeof newString)
 
 // Write a JavaScript program to convert a string to the number.
+const stringToInt = (word) => {
+  let change = parseInt(word);
+  return change;
+  // console.log(change)
+}
+let newInt = stringToInt('7');
+console.log(newInt);
+console.log(typeof newInt)
 
 
 
